0368a05271445962934b7b747cd9684c67a6444f
3c0n0mist <47517636+3c0n0mist@users.noreply.github.com>
2019-09-22 11:11:44 -0700

    created the python program

---
+++ free.py
@@ -0,0 +1,13 @@
+#! python3
+#program to socialize
+#opens all social media websites used often
+import webbrowser
+
+webbrowser.open('http://instagram.com',1,True)
+webbrowser.open('http://facebook.com',1,False)
+webbrowser.open('http://linkedin.com',1,False)
+webbrowser.open('http://youtube.com',1,False)
+webbrowser.open('http://twitter.com',1,False)
+
+    
+
